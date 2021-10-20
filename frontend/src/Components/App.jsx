51977caceb9b160d27449cb1d30c0f486371c01c
martinharts <89169036+martinharts@users.noreply.github.com>
2021-10-19 22:43:16 -0400

    added dependencies and created component for VideoPlayer

---
+++ frontend/src/Components/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import VideoPlayer from "./VideoPlayer/VideoPlayer";
+
+class App extends Component{
+    state = { }
+    render(){
+        return(
+        <VideoPlayer />
+        )
+    }
+}
+
+export default App
---
+++ frontend/src/Components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Col from 'react-bootstrap/Col';
+import Spinner from 'react-bootstrap/Spinner';
+import Button from 'react-bootstrap/Button';
+
+
+const VideoPlayer = ({videoID}) => {
+    if (!videoID){
+        return (
+            <col className='loading-spinner'>
+                <Button variant="dark" disabled >
+                    <h3>Search for video</h3>
+                    <Spinner 
+                    as="span"
+                    animation ="grow"
+                    size ="lg"
+                    role="status"
+                    aria-hidden="true"
+                    />
+                    <h3>Loading...</h3>
+                </Button>
+            </col>
+        );
+    }
+    return(
+        <Col md={10} xs={12} className="video-player mx-auto text-center py-2">
+            <div className='ratio ratio-16x9'>
+                <iframe
+                title = {videoID}
+                className="video-iframe border border-danger border-4 rounded-3 shadow"
+                src={`https://www.youtube.com/embed/${videoId}`}
+                allowFullScreen
+                />
+            </div>
+        </Col>
+    );
+}
+
+export default VideoPlayer
