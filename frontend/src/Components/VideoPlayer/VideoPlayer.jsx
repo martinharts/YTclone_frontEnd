283090d5be4f7ae389074d6d09dcf29863aef3f7
martinharts <89169036+martinharts@users.noreply.github.com>
2021-10-20 19:44:33 -0400

    tested video player. working code. working on styling and creating comments

--- frontend/src/Components/VideoPlayer/VideoPlayer.jsx
+++ frontend/src/Components/VideoPlayer/VideoPlayer.jsx
@@ -5,26 +5,26 @@ import Button from 'react-bootstrap/Button';
 
 
 const VideoPlayer = ({videoID}) => {
-    const videoId = "wbernJatch?v=s4Bix"
-    if (!videoID){
-        return (
-            <Col className='loading-spinner'>
-                <Button variant="dark" disabled >
-                    <h3>Search for video</h3>
-                    <Spinner 
-                    as="span"
-                    animation ="grow"
-                    size ="lg"
-                    role="status"
-                    aria-hidden="true"
-                    />
-                    <h3>Loading...</h3>
-                </Button>
-            </Col>
-        );
-    }
+    const videoId = "M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+    // if (!videoID){
+    //     return (
+    //         <Col className='loading-spinner'>
+    //             <Button variant="dark" disabled >
+    //                 <h3>Search for video</h3>
+    //                 <Spinner 
+    //                 as="span"
+    //                 animation ="grow"
+    //                 size ="lg"
+    //                 role="status"
+    //                 aria-hidden="true"
+    //                 />
+    //                 <h3>Loading...</h3>
+    //             </Button>
+    //         </Col>
+    //     );
+    // }
     return(
-        <Col md={10} xs={12} className="video-player mx-auto text-center py-2">
+        <Col md={8} xs={10} className="video-player mx-auto text-center py-2">
             <div className='ratio ratio-16x9'>
                 <iframe
                 title = {videoID}
