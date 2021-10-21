813641ce51e4b4f2f49bb8173b6341cb5c332910
martinharts <89169036+martinharts@users.noreply.github.com>
2021-10-21 19:17:32 -0400

    made changes to CommentCreator, Comments, and added Replies component. working on connecting to mongoDB api

--- frontend/src/Components/App.jsx
+++ frontend/src/Components/App.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import VideoPlayer from "./VideoPlayer/VideoPlayer";
-import CommentCreator from "./CommentCreator/CommentCreator";
+// import CommentCreator from "./CommentCreator/CommentCreator";
 import Comments from "./Comments/Comments";
 import axios from "axios";
 import Header from "./Header/Header";
+
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      comments: [],
+        videoId: null,
+        videoTitle: null,
+        videoDescription: null,
+        relatedVideos: [],
     };
   }
   createComment = async (comment, videoId) => {
@@ -33,10 +38,9 @@ class App extends Component {
           </div>
         </div>
 
-        <div className="row">
-          <CommentCreator createComment={this.createComment} />
-          <Comments comments={this.state.comments} />
-        </div>
+        <div className='col-8 col-md-9'>
+              <Comments videoId={this.state.videoId} replyId={null} key={this.state.videoId}/>
+            </div>
       </div>
     );
   }
--- frontend/src/Components/CommentCreator/CommentCreator.jsx
+++ frontend/src/Components/CommentCreator/CommentCreator.jsx
@@ -1,40 +1,67 @@
 import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
-
-class CommentCreator extends Component {
+class CommentCreator extends Component{
     constructor(props){
         super(props);
         this.state = {
-            body:"",
-            videoID: ""
+            comment: "",
+            name: "",
         }
     }
-    
+
+    addComment = async () => {
+        let comment = {
+            name: this.state.name,
+            comment: this.state.comment,
+            videoId: this.props.videoId,
+            replyToId: this.props.replyId
+        }
+        try {
+            await axios.post('http://localhost:5000/comments/post', comment);
+            this.setState({
+                name: '',
+                comment: '',
+            });
+            this.props.refresh();
+            if (this.props.hide){
+                this.props.hide()
+            }
+        }
+        catch (err) {
+            alert(err);
+        }
+    }
+
     handleChange = (event) => {
         this.setState({
             [event.target.name]: event.target.value
         })
     }
+
     handleSubmit = (event) => {
-        event.preventDefault()
-        console.log(this.state)
-        // this.props.CommentCreator(this.state, this.props.videoId)
-        this.setState({
-            body: "",
-            videoID: ""
-        })
+        event.preventDefault();
+        this.addComment();
     }
-    render(){
-        return(
-            <div>
-                <h3>Add Comment</h3>
-                <form onSubmit={(event)=> this.handleSubmit(event)}>
-                    <input placeholder="Comment" name = "body" onChange={this.handleChange} value={this.state.body}/>
-                    <button type="Submit">Submit</button>
-                </form>
+
+    render() { 
+        return ( 
+            <div className="container jumbotron small" style={{maxHeight: '30vh'}}>
+                <h3>Comment</h3>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group  controlId="name">
+                        <Form.Control type="text" placeholder="Enter your name" name='name' onChange={this.handleChange} value={this.state.name}/>
+                    </Form.Group>  
+                    <Form.Group controlId="comment">
+                        <Form.Control type="text" placeholder="Leave a comment..." name='comment' value={this.state.comment} onChange={this.handleChange}/>
+                    </Form.Group>           
+                    <Button variant="primary" type="submit">Add Comment</Button>
+                </Form>
             </div>
-        );
+         );
     }
 }
 
-export default CommentCreator
+export default CommentCreator;
