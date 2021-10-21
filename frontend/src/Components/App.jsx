c91c5471291339802c6512bda74277d07369bf0c
techniqueo <67283557+techniqueo@users.noreply.github.com>
2021-10-20 21:05:22 -0500

    Worked on the Header

--- frontend/src/Components/App.jsx
+++ frontend/src/Components/App.jsx
@@ -1,42 +1,45 @@
 import React, { Component } from "react";
 import VideoPlayer from "./VideoPlayer/VideoPlayer";
 import CommentCreator from "./CommentCreator/CommentCreator";
-import Comments from "./Comments/Comments"
-import axios from 'axios';
-
-class App extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            comments:[]
-        }
-    }
-    createComment = async (comment,videoId) => {
-        comment.videoId = videoId
-        let response = await axios.post('http://localhost:5000/comments/',comment)
-        let newComments = this.state.comments;
-        newComments.push(response.data);
-        console.log(response)
-        this.setState({
-          comments: newComments
-        })
-      }
-    render(){
-        return(
-        <div>
-            <div className='row bg-dark'>
-                <div className='col'>
-                    <VideoPlayer />
-                </div>
-          </div>
-
-          <div className='row'>
-              <CommentCreator createComment ={this.createComment}/>
-              <Comments comments = {this.state.comments} />
+import Comments from "./Comments/Comments";
+import axios from "axios";
+import Header from "./Header/Header";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: [],
+    };
+  }
+  createComment = async (comment, videoId) => {
+    comment.videoId = videoId;
+    let response = await axios.post("http://localhost:5000/comments/", comment);
+    let newComments = this.state.comments;
+    newComments.push(response.data);
+    console.log(response);
+    this.setState({
+      comments: newComments,
+    });
+  };
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <Header />
+        </div>
+        <div className="row bg-dark">
+          <div className="col">
+            <VideoPlayer />
           </div>
         </div>
-        )
-    }
+
+        <div className="row">
+          <CommentCreator createComment={this.createComment} />
+          <Comments comments={this.state.comments} />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
---
+++ frontend/src/Components/Header/Header.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import axios from "axios";
+import "./Header.css";
+
+const Header = () => {
+  const [inputSearch, setInputSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`https://www.googleapis.com/youtube/v3/search?q=${inputSearch}&key=`)
+      .then((response) => setInputSearch(response.data["items"]));
+  }, [inputSearch]);
+
+  return (
+    <div className="header">
+      <div className="header__left">
+        <MenuIcon />
+
+        <img
+          className="header__logo"
+          src="../images/logo.png"
+          alt="YouTube Logo"
+        />
+      </div>
+
+      <div className="header__input">
+        <input type="text" placeholder="Search" />
+        <SearchIcon className="header__inputButton" type="submit" />
+      </div>
+
+      <div className="header__right">
+        <VideoCallIcon className="header__icon" />
+        <AppsIcon className="header__icon" />
+        <NotificationsIcon className="header__icon" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
